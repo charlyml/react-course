cb37065a7d4248b531fe82ea3cdfabcecccaa623
Carlos Maldonado <cmaldonadol@outlook.com>
2019-06-17 18:01:39 -0500

    Test finished

--- src/App.js
+++ src/App.js
@@ -1,70 +1,26 @@
 import React, { useState } from 'react';
 import './App.css';
-import Person from './Person/Person';
+
+import UserInput from './UserInput/UserInput';
+import UserOutput from './UserOutput/UserOutput';
 
 function App() {
-  const [ personsState, setPersonsState ] = useState({
-    persons: [
-      { name: 'Max', age: 25 },
-      { name: 'Manu', age: 26 },
-      { name: 'Json', age: 24 }
-    ],
+  const [usernameState, setUsernameState] = useState({
+    username: "Super Carlos"
   });
 
-  const [otherState, setOtherState] = useState("Some other value");
-
-  console.log(personsState, otherState);
-
-  const switchNameHandler = (newName) => {
-    setPersonsState({
-      persons: [
-        { name: newName, age: 28 },
-        { name: 'Carlos', age: 29 },
-        { name: 'Anel', age: 27 },
-      ]
+  const usernameChangedHandler = (event) => {
+    setUsernameState({
+      username: event.target.value
     });
-  }
-
-  const nameChangedHandler = (event) => {
-    setPersonsState({
-      persons: [
-        { name: "Max", age: 28 },
-        { name: event.target.value, age: 29 },
-        { name: 'Anel', age: 26 },
-      ]
-    });
-  }
-
-  const style = {
-    backgroundColor: 'white',
-    font: 'inherit',
-    border: '1px solid blue',
-    padding: '8px',
-    cursor: 'pointer'
   }
 
   return (
     <div className="App">
-      <h1>Hi, I'm a React App</h1>
-      <p>This is really working</p>
-      <button
-        onClick={ switchNameHandler.bind(this, 'Max!') }
-        style={style}>
-          Switch name
-        </button>
-      <Person
-        name={ personsState.persons[0].name }
-        age={ personsState.persons[0].age }/>
-      <Person
-        name={ personsState.persons[1].name }
-        age={ personsState.persons[1].age }
-        click={ switchNameHandler.bind(this, 'Max') }
-        changed={nameChangedHandler}>
-        My hobbies: Racing
-      </Person>
-      <Person
-        name={ personsState.persons[2].name }
-        age={ personsState.persons[2].age }/>
+      <UserInput currentName={usernameState.username} changed={usernameChangedHandler} />
+      <UserOutput username={usernameState.username} />
+      <UserOutput username={usernameState.username} />
+      <UserOutput username="Anel" />
     </div>
   );
 }
