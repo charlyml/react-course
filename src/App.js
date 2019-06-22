846eb8435686b592f49b9c5a2b94b5a7a78b9b90
Carlos Maldonado <cmaldonadol@outlook.com>
2019-06-22 12:56:08 -0500

    Changed to css modules

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-import "./App.css";
+import styles from "./App.module.css";
 import Person from "./Person/Person";
 
 class App extends Component {
@@ -53,15 +53,8 @@ class App extends Component {
   };
 
   render() {
-    const style = {
-      backgroundColor: "green",
-      font: "inherit",
-      border: "1px solid blue",
-      padding: "8px",
-      cursor: "pointer",
-    };
-
     let persons = null;
+    let btnClass = "";
 
     if (this.state.showPersons) {
       persons = (
@@ -80,28 +73,28 @@ class App extends Component {
         </div>
       );
 
-      style.backgroundColor = "red";
+      btnClass = styles.Red;
     }
 
     let classes = [];
 
     if (this.state.persons.length <= 2) {
-      classes.push("red");
+      classes.push(styles.red);
     }
 
     if (this.state.persons.length <= 1) {
-      classes.push("bold");
+      classes.push(styles.bold);
     }
 
     return (
-        <div className="App">
-          <h1>Hi, I'm a React App</h1>
-          <p className={classes.join(" ")}>This is really working</p>
-          <button onClick={this.togglePersonHandler} style={style}>
-            Switch name
-          </button>
-          {persons}
-        </div>
+      <div className={styles.App}>
+        <h1>Hi, I'm a React App</h1>
+        <p className={classes.join(" ")}>This is really working</p>
+        <button onClick={this.togglePersonHandler} className={btnClass}>
+          Switch name
+        </button>
+        {persons}
+      </div>
     );
   }
 }
