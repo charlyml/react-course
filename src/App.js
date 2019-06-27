bccc15b140dc3d888c9dc9c8bd5a526fcd3567c4
Carlos Maldonado <cmaldonadol@outlook.com>
2019-06-27 13:28:21 -0500

    Burger components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 
-import styles from "./App.module.css";
+import Layout from './components/Layout/Layout'
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <p>Hello world!</p>
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
       </div>
     );
   }
